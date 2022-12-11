e476c7c96cbdd8749417a19c47e8807ab6b89fbd
Vladimir Jovanović <114678399+DobarBREND@users.noreply.github.com>
2022-12-11 23:05:14 +0100

    Add files via upload

---
+++ js/myscripts.js
@@ -0,0 +1,142 @@
+	function inputMargin11() {
+			
+			var inputPadding = document.getElementById("padding11").value;
+			var inputMargin = document.getElementById("margin11").value;
+			var inputBorderWidth = document.getElementById("border11").value;
+			document.getElementById("flex11").style.margin = inputMargin;
+			document.getElementById("flex11").style.borderWidth = inputBorderWidth;
+			document.getElementById("flex11").style.padding = inputPadding;
+			document.getElementById("flex11").style.boxShadow = "inset " + "0 0 0 " + inputPadding + " lightGreen";
+			}
+
+	function inputMargin12() {
+			var inputMargin = document.getElementById("margin12").value;
+			var inputBorderWidth = document.getElementById("border12").value;
+			var inputPadding = document.getElementById("padding12").value;
+			document.getElementById("flex12").style.margin = inputMargin;
+			document.getElementById("flex12").style.borderWidth = inputBorderWidth;
+			document.getElementById("flex12").style.padding = inputPadding;
+			document.getElementById("flex12").style.boxShadow = "inset " + "0 0 0 " + inputPadding + " lightGreen";
+			}
+			
+	function inputMargin13() {
+			var inputMargin = document.getElementById("margin13").value;
+			var inputBorderWidth = document.getElementById("border13").value;
+			var inputPadding = document.getElementById("padding13").value;
+			document.getElementById("flex13").style.margin = inputMargin;
+			document.getElementById("flex13").style.borderWidth = inputBorderWidth;
+			document.getElementById("flex13").style.padding = inputPadding;
+			document.getElementById("flex13").style.boxShadow = "inset " + "0 0 0 " + inputPadding + " lightGreen";
+			}
+			
+	function inputMargin14() {
+			var inputMargin = document.getElementById("margin14").value;
+			var inputBorderWidth = document.getElementById("border14").value;
+			var inputPadding = document.getElementById("padding14").value;
+			document.getElementById("flex14").style.margin = inputMargin;
+			document.getElementById("flex14").style.borderWidth = inputBorderWidth;
+			document.getElementById("flex14").style.padding = inputPadding;
+			document.getElementById("flex14").style.boxShadow = "inset " + "0 0 0 " + inputPadding + " lightGreen";
+			}
+			
+	function inputMargin21() {
+			var inputMargin = document.getElementById("margin21").value;
+			var inputBorderWidth = document.getElementById("border21").value;
+			var inputPadding = document.getElementById("padding21").value;
+			document.getElementById("flex21").style.margin = inputMargin;
+			document.getElementById("flex21").style.borderWidth = inputBorderWidth;
+			document.getElementById("flex21").style.padding = inputPadding;
+			document.getElementById("flex21").style.boxShadow = "inset " + "0 0 0 " + inputPadding + " lightGreen";
+			}
+			
+	function inputMargin22() {
+			var inputMargin = document.getElementById("margin22").value;
+			var inputBorderWidth = document.getElementById("border22").value;
+			var inputPadding = document.getElementById("padding22").value;
+			document.getElementById("flex22").style.margin = inputMargin;
+			document.getElementById("flex22").style.borderWidth = inputBorderWidth;
+			document.getElementById("flex22").style.padding = inputPadding;
+			document.getElementById("flex22").style.boxShadow = "inset " + "0 0 0 " + inputPadding + " lightGreen";
+			}
+			
+	function inputMargin23() {
+			var inputMargin = document.getElementById("margin23").value;
+			var inputBorderWidth = document.getElementById("border23").value;
+			var inputPadding = document.getElementById("padding23").value;
+			document.getElementById("flex23").style.margin = inputMargin;
+			document.getElementById("flex23").style.borderWidth = inputBorderWidth;
+			document.getElementById("flex23").style.padding = inputPadding;
+			document.getElementById("flex23").style.boxShadow = "inset " + "0 0 0 " + inputPadding + " lightGreen";
+			}
+			
+	function inputMargin24() {
+			var inputMargin = document.getElementById("margin24").value;
+			var inputBorderWidth = document.getElementById("border24").value;
+			var inputPadding = document.getElementById("padding24").value;
+			document.getElementById("flex24").style.margin = inputMargin;
+			document.getElementById("flex24").style.borderWidth = inputBorderWidth;
+			document.getElementById("flex24").style.padding = inputPadding;
+			document.getElementById("flex24").style.boxShadow = "inset " + "0 0 0 " + inputPadding + " lightGreen";
+			}
+			
+	function inputMargin31() {
+			var inputMargin = document.getElementById("margin31").value;
+			var inputBorderWidth = document.getElementById("border31").value;
+			var inputPadding = document.getElementById("padding31").value;
+			document.getElementById("flex31").style.margin = inputMargin;
+			document.getElementById("flex31").style.borderWidth = inputBorderWidth;
+			document.getElementById("flex31").style.padding = inputPadding;
+			document.getElementById("flex31").style.boxShadow = "inset " + "0 0 0 " + inputPadding + " lightGreen";
+			}
+			
+	function inputMargin32() {
+			var inputMargin = document.getElementById("margin32").value;
+			var inputBorderWidth = document.getElementById("border32").value;
+			var inputPadding = document.getElementById("padding32").value;
+			document.getElementById("flex32").style.margin = inputMargin;
+			document.getElementById("flex32").style.borderWidth = inputBorderWidth;
+			document.getElementById("flex32").style.padding = inputPadding;
+			document.getElementById("flex32").style.boxShadow = "inset " + "0 0 0 " + inputPadding + " lightGreen";
+			}
+			
+	function inputMargin33() {
+			var inputMargin = document.getElementById("margin33").value;
+			var inputBorderWidth = document.getElementById("border33").value;
+			var inputPadding = document.getElementById("padding33").value;
+			document.getElementById("flex33").style.margin = inputMargin;
+			document.getElementById("flex33").style.borderWidth = inputBorderWidth;
+			document.getElementById("flex33").style.padding = inputPadding;
+			document.getElementById("flex33").style.boxShadow = "inset " + "0 0 0 " + inputPadding + " lightGreen";
+			}
+			
+	function inputMargin34() {
+			var inputMargin = document.getElementById("margin34").value;
+			var inputBorderWidth = document.getElementById("border34").value;
+			var inputPadding = document.getElementById("padding34").value;
+			document.getElementById("flex34").style.margin = inputMargin;
+			document.getElementById("flex34").style.borderWidth = inputBorderWidth;
+			document.getElementById("flex34").style.padding = inputPadding;
+			document.getElementById("flex34").style.boxShadow = "inset " + "0 0 0 " + inputPadding + " lightGreen";
+			}
+			
+	function flexContainerChange() {
+			let inputFlexDirection = document.getElementById("flex-direction-id").value;
+			let inputFlexWrap = document.getElementById("flex-wrap-id").value;
+			let inputJustifyContent = document.getElementById("justify-content-id").value;
+			let inputAlignItems = document.getElementById("align-items-id").value;
+			let inputAlignContent = document.getElementById("align-content-id").value;
+			document.getElementById("flex-container").style.flexDirection = inputFlexDirection;
+			document.getElementById("flex-container").style.flexWrap = inputFlexWrap;
+			document.getElementById("flex-container").style.justifyContent = inputJustifyContent;
+			document.getElementById("flex-container").style.alignItems = inputAlignItems;
+			document.getElementById("flex-container").style.alignContent = inputAlignContent;
+			}
+			
+	/*function displayResult(){
+			let selectElement = document.getElementById('flex-direction-id');
+			let optionNames = [...selectElement.options].map(o => o.text);
+			console.log(optionNames);
+			}*/
+	
+		
+		
